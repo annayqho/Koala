a9d37cb2cd8e21253a1fa7cab497491df3e3611d
annayqho <annayqho@gmail.com>
2019-06-26 22:30:48 -0700

    search for koala analogs in the first window

--- code/query.py
+++ code/query.py
@@ -6,15 +6,68 @@ Criteria:
     - jdendhist - jdstarthst < 1 year
 """
 
+import numpy as np
+import pandas
+import sys
+sys.path.append("/Users/annaho/Dropbox/Projects/Research/ZTF_fast_transient_search/code")
+#from print_diagnostics import *
 from kowalski_login import logon
 
 
+def get_query(search_start, search_end):
+    query = """db['ZTF_alerts'].find_one(
+        {'candidate.jd':
+            {'$gt': %(startsearch)f, '$lt': %(endsearch)f},
+         'candidate.field': {'$eq': 554},
+         'candidate.rb': {'$gt': 0.3},
+         'candidate.ndethist': {'$gt': 2},
+         'candidate.magpsf': {'$lt': 20},
+         'candidate.isdiffpos': {'$in': ['1', 't']}},
+         {'objectId'}
+        )""" %( {'startsearch': search_start, 'endsearch': search_end})
+    return query
+
+
+def get_details(name):
+    query = """db['ZTF_alerts'].find_one(
+            {'objectId': {'$eq': '%s'}},
+            {'candidate.ssdistnr', 'candidate.distpsnr1', 'candidate.sgscore1'}
+        )""" %name
+    query_result = s.query(
+            {'query_type': 'general_search', 'query': query})
+    out = query_result['result_data']['query_result']
+    ssdistnr = out['candidate']['ssdistnr']
+    distnr = out['candidate']['distpsnr1']
+    sgscore = out['candidate']['sgscore1']
+    return ssdistnr, distnr, sgscore
+
+
 if __name__=="__main__":
+    # The very first observation of this field: 58345.44143477606
+    # The last observation of this field before the break: 58386.4729638
+    search_start = 2458345.94143478
+    search_end = 2458386.9729638
+
     s = logon()
-    q = {"query_type": "general_search", 
-         "query": "db['ZTF_alerts'].aggregate([{'$group':{'_id':'$objectId', 
-                                                          'count':{'$sum':{'$cond':[{'$gt':['$candidate.rb',0.5]}, 1, 0]} } } }, 
-                                               {'$match':{'count':{'$gt':20}}}, 
-                                               {'$project':{'objectId':'$_id','_id':0}}], allowDiskUse=True)" }
-    r = s.query(query=q)
-    print(r['result']['query_result'])
+    #q = get_query(search_start, search_end)
+    #r = s.query(query=q)
+    #print(r['result']['query_result'])
+    names = []
+    dat = pandas.read_json("../docs/later_query.json")
+    for item in dat['query_result']:
+        name = item['objectId']
+        names.append(name)
+        #names.append(item['objectId'])
+    names = np.unique(np.array(names))
+    print(len(names))
+
+    keep = []
+    for ii,name in enumerate(names):
+        print(ii)
+        ssdistnr, distnr, sgscore = get_details(name)
+        notrock = ssdistnr<0
+        star = np.logical_and(distnr<1,sgscore>0.8)
+        if np.logical_and(notrock, ~star):
+            keep.append(name)
+
+    np.savetxt("keep_2.txt", keep, dtype=str)
--- code/survey_summary.py
+++ code/survey_summary.py
@@ -1,5 +1,40 @@
 """ Use ZTFQuery to get basic info about the nightly cadence survey """
 
-from ztfquery import query
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import rc
+rc("font", family="serif")
+rc("text", usetex=True)
+from astropy.time import Time
+from sqlalchemy import create_engine
+import pandas as pd
 
+# These are all the 1DC observations
+path = '/Users/annaho/Dropbox/Projects/Research/Koala/docs/anna.db'
+engine = create_engine(f'sqlite:///{path}')
 
+df = pd.read_sql('Summary', engine)
+
+# Eric says: note that the limiting mags are not meaningful
+
+# Load in the relevant info
+mjd = df['expMJD']
+fields = df['fieldID']
+filt = df['filter']
+
+# Use g-band and Field ID 554 (field with The Koala)
+choose = np.logical_and(fields == 554, filt=='g')
+g_obsdates = mjd[choose].values
+g_dt = g_obsdates[1:]-g_obsdates[0:-1]
+
+choose = np.logical_and(fields == 554, filt=='r')
+r_obsdates = mjd[choose].values
+r_dt = r_obsdates[1:]-r_obsdates[0:-1]
+
+plt.figure(figsize=(6,4))
+plt.hist(g_dt, range=(0,12), histtype='step', color='k', density=True)
+plt.xlabel("Days before next obs", fontsize=16)
+plt.ylabel("Fraction of $g$-band observations", fontsize=16)
+plt.tick_params(labelsize=14)
+plt.tight_layout()
+plt.show()
