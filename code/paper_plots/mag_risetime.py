d7b3546aed58f757d546c271709a15df8b941e54
annayqho <annayqho@gmail.com>
2019-12-28 18:53:58 -0800

    Add the Ibn

--- code/paper_plots/mag_risetime.py
+++ code/paper_plots/mag_risetime.py
@@ -21,19 +21,40 @@ textsize=14
 def sn2018gep(ax):
     """ Rise time and peak g-band mag for SN2018gep """
     trise = 3
-    plum = -19.9
+    plum = -19.7
 
     ax.scatter(
             trise, plum, marker='o', c='k') 
-    ax.text(trise*1.1, plum, "ZTF18abukavn", fontsize=textsize,
-            verticalalignment='center')
+    ax.text(trise/1.05, plum, "ZTF18abukavn (Ic-BL)", fontsize=textsize,
+            verticalalignment='center',
+            horizontalalignment='right')
 
 
-def koala(ax):
-    """ Rise time and peak g-band mag for SN2018gep """
+def ztf(ax):
+    """ Rise time and peak g-band mag for the three new ZTF sources """
+
+    # ZTF18achdidy
+    trise = 2
+    plum = -20.2
+    ax.scatter(
+            trise+5, plum, marker='o', c='k') 
+    ax.arrow(
+           trise+5, plum, -5, 0, color='k', head_width=0.1, head_length=0.1)
+    ax.text(trise*1.1, plum, "ZTF18achdidy (SLSN-I)", fontsize=textsize,
+            verticalalignment='bottom')
+
+    # ZTF19aapfmki
+    trise = 4.6
+    plum = -19.5
+    ax.scatter(
+            trise, plum, marker='o', c='k') 
+    ax.text(trise*1.08, plum*1.001, "ZTF19aapfmki (Ibn)", fontsize=textsize,
+            verticalalignment='bottom', horizontalalignment='right')
+
+
+    # ZTF18abvkwla
     trise = 1.3
     plum = -20.9
-
     ax.scatter(
             trise, plum, marker='o', c='k') 
     ax.text(trise*1.1, plum, "ZTF18abvkwla", fontsize=textsize,
@@ -44,9 +65,13 @@ def at2018cow(ax):
     """ Rise time and peak mag """
     x = 0.4
     y = -20.6
-    #ax.errorbar(
-    #        x, y, xerr=0.5, yerr=0.03, c='k', marker='o')
-    ax.text(x, y*1.005, "18cow", fontsize=textsize)
+    ax.errorbar(
+            x, y, yerr=0.03, c='k', marker='o')
+    ax.arrow(
+           x, y, -0.1, 0, color='k', head_width=0.1, head_length=0.1)
+    ax.text(x, y/1.003, "18cow", fontsize=textsize,
+            horizontalalignment='center',
+            verticalalignment='top')
     
 
 def iptf16asu(ax):
@@ -60,7 +85,7 @@ def iptf16asu(ax):
     """
     # from the paper: 3.97 \pm 0.19 days in g-band
     # peak is -20.4
-    trise = 2.7
+    trise = 1.25
 
     # we do resolve Mg, but Lbol is a strict lower limit
     plum = -20.4
@@ -68,7 +93,9 @@ def iptf16asu(ax):
     ax.errorbar(
             trise, plum, yerr=0.1, xerr=0.19, marker='o', c='k')
     ax.text(
-            trise, plum*1.005, "iPTF16asu", fontsize=textsize)
+            trise*1.05, plum, "iPTF16asu (Ic-BL)", fontsize=textsize,
+            verticalalignment='bottom',
+            horizontalalignment='left')
 
 
 def rest2018(ax):
@@ -89,7 +116,9 @@ def drout(ax):
     y = -19.5
     ax.errorbar(
            x, y, xerr=0.1, yerr=0.08, c='k', marker='o')
-    ax.text(x, y*1.005, "11qr", fontsize=textsize)
+    ax.text(x/1.05, y, "11qr", fontsize=textsize,
+            verticalalignment='center',
+            horizontalalignment='right')
     
 
 def arcavi(ax):
@@ -99,7 +128,7 @@ def arcavi(ax):
     ax.errorbar(
            x, y, yerr=0.1, c='k', marker='o')
     ax.arrow(
-           x, y, -0.3, 0, color='k', head_width=0.1, head_length=0.1)
+           x, y, -0.1, 0, color='k', head_width=0.1, head_length=0.1)
     ax.text(x*1.05, y, "04D4ec", fontsize=textsize,
         horizontalalignment='left')
 
@@ -107,7 +136,8 @@ def arcavi(ax):
     y = -20.08
     ax.errorbar(
            x, y, yerr=0.1, c='k', marker='o')
-    ax.text(x, y*1.005, "05D2bk", fontsize=textsize)
+    ax.text(x*1.05, y, "05D2bk", fontsize=textsize,
+    verticalalignment='center', horizontalalignment='left')
 
     x = 4.6
     y = -20.7
@@ -118,9 +148,20 @@ def arcavi(ax):
             fontsize=textsize, horizontalalignment='right')
 
 
-fig,ax = plt.subplots(1,1,figsize=(5,5))
+def ibn(ax):
+    """ using rest-frame g-band """
+    x = 2.3
+    y = -21.3
+    ax.scatter(
+           x, y, c='k', marker='o')
+    ax.text(x*1.05, y, "iPTF15ul (Ibn)", fontsize=textsize,
+        horizontalalignment='left')
+
+
+fig,ax = plt.subplots(1,1,figsize=(6,5))
+ibn(ax)
 sn2018gep(ax)
-koala(ax)
+ztf(ax)
 at2018cow(ax)
 iptf16asu(ax)
 drout(ax)
@@ -128,7 +169,7 @@ arcavi(ax)
 
 ax.set_ylabel("Peak Mag (Rest-frame $g$-band)", fontsize=16)
 ax.set_xlim(0,5)
-ax.set_ylim(-21, -19.5)
+ax.set_ylim(-21.4, -19.4)
 #ax.set_xscale('log')
 ax.invert_yaxis()
 ax.set_xlabel(
@@ -138,6 +179,6 @@ ax.xaxis.set_tick_params(labelsize=14)
 ax.yaxis.set_tick_params(labelsize=14)
 
 fig.tight_layout()
-#plt.savefig("lum_rise.png", dpi=200)
+plt.savefig("lum_rise.png", dpi=200)
 
-plt.show()
+#plt.show()
